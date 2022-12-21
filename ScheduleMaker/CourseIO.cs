a3f4400daf0bb7d2481023b1b295b3d1a8cbfa2f
Axale <38008781+Axale@users.noreply.github.com>
2022-12-21 18:07:33 -0500

    Initial commit

---
+++ ScheduleMaker/CourseIO.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ScheduleMaker
+{
+    internal class CourseIO {
+        string Path;
+        FileStream fs;
+        string filestring;
+        public bool Failed;
+
+
+        public CourseIO(string Given_Path) {
+            Path = Given_Path;
+            if (!File.Exists(Path)) {
+
+                if (!CreateFile()) {
+                    Failed = true;
+                }
+            }
+            else {
+                if (!OpenFile()) {
+                    Failed = true;
+                }
+            }
+
+        }
+
+        // Creates a file, checking success
+        private bool CreateFile()
+        {
+            bool success = true;
+            try { 
+                fs = new FileStream(Path, FileMode.Create);
+            } catch (IOException) {
+                success = false;
+            } finally {
+                if(!success)
+                {
+                    fs.Close();
+                }
+            }
+
+            return success;
+        }
+
+        private bool OpenFile() {
+            bool success = true;
+            try {
+                fs = new FileStream(Path, FileMode.Open);
+            }
+            catch (IOException) {
+                success = false;
+            } finally {
+                if(!success) {
+                    fs.Close();
+                }
+            }
+
+            return success;
+        }
+    }
+}
---
+++ ScheduleMaker/CourseObj.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScheduleMaker {
+    internal class CourseObj {
+        public string Name;
+        public string CreditHours;
+        public string NumberOfMeetings;
+        public List<MeetingTime> MeetingTimes;
+
+        public class MeetingTime {
+            public string StartTime;
+            public string EndTime;
+            public string Type;
+
+            public MeetingTime(string g_Start, string g_End, string g_Type) {
+                StartTime = g_Start;
+                EndTime = g_End;
+                Type = g_Type;
+            }
+        }
+    }
+}
